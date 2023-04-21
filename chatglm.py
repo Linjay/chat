addfd8eb4474e9814413c7ff68de5ccd3a2e23c2
linjayzhang <linjayzhang326@gmail.com>
2023-04-22 00:51:36 +0800

    222

--- chatglm.py
+++ chatglm.py
@@ -12,7 +12,20 @@ s.verify = False
 urllib3.disable_warnings()
 app = Flask(__name__)
 
-chatgpt_conversation=[]
+chat_holder={
+    "cid5Jo6E9HFyq9h53OKZLga0Q==":{
+        "history":[],
+        "dingToken":"6aa3cf223f652ff19d48518f9fdd7f7970def822e19a4f30557c6d05d75fbe16"
+    },
+    "cidbRFEZ6AtbzXd/9vjVZpLFw==":{
+        "history":[],
+        "dingToken":"0551bff6504ccab407014fc4d8a84c6e8657609f17668fce2c05e2828a903122"
+    },
+    "cid3woGpG5a4GbH/U88zZVH+w==":{
+        "history":[],
+        "dingToken":"6d796eac1cb8ee7affe06743871a2157c48251149236fa97d53cea2c95787a84"
+    },
+}
 
 @app.route("/")
 def hi():
@@ -20,49 +33,46 @@ def hi():
 
 @app.route("/webhook/event",methods=['POST'])
 def event():
-    global chatgpt_conversation
-    proxies={'http':'http://192.168.50.4:7890','https':'https://192.168.50.4:7890'}
-    headers={'Content-Type': 'application/json','Authorization': 'Bearer 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'}
+    global chat_holder
+    headers={'Content-Type': 'application/json'}
     json_data=json.loads(request.data)
     print(json_data)
-    chatgpt_conversation.append({"role": "user", "content": json_data['text']['content']})
+    asker=json_data['senderNick']
+    conversationId = json_data['conversationId']
+    if conversationId not in chat_holder.keys():
+        print("invalid conversation:"+conversationId)
     json_message={
-    "model": "gpt-3.5-turbo",
-    "messages": chatgpt_conversation}
+        "history": chat_holder[conversationId]['history'],
+        "prompt": json_data['text']['content'],
+        "max_length":20480
+    }
     print("send message to gpt")
     print(json_message)
-    chatgpt_response=requests.post('https://api.openai.com/v1/chat/completions',headers=headers,json=json_message,proxies={})
-    print("chatgpt_response:")
-    print(chatgpt_response)
-    chatgpt_conversation.append(chatgpt_response.json()['choices'][0]['message'])
-    answer=chatgpt_response.json()['choices'][0]['message']['content']
-    print("----"*20)
-    print(answer)
-    print("----"*20)
+
+    answer="不知道！"
+    try:
+        chatgpt_response=requests.post('http://192.168.50.185:8000',headers=headers,json=json_message,proxies={})
+        print("chatgpt_response:")
+        print(chatgpt_response.json())
+        answer=chatgpt_response.json()['response']
+        chat_holder[conversationId]['history'] = chatgpt_response.json()['history']
+
+        print("----"*20)
+        print(answer)
+        print("----"*20)
+    except:
+        answer += "我崩溃了。。。呜呜呜"
+        print("An exception occurred")
+
     headers2={'Content-Type':'application/json'}
-    json_sendmessages={"msgtype": "text","text": {"content":answer+'\r\nconversation:'+str(len(chatgpt_conversation))}}
-    url='https://oapi.dingtalk.com/robot/send?access_token=6aa3cf223f652ff19d48518f9fdd7f7970def822e19a4f30557c6d05d75fbe16'
+    json_sendmessages={"msgtype": "text","text": {"content":asker+"你好!\r\n"+answer+'\r\nconversation:'+str(len(chat_holder[conversationId]['history']))}}
+    url='https://oapi.dingtalk.com/robot/send?access_token='+chat_holder[conversationId]['dingToken']
     response=requests.post(url,headers=headers2,json=json_sendmessages)
     print(response.text)
-    if len(chatgpt_conversation)>16:
-        chatgpt_conversation=[]
+
+    if len(chat_holder[conversationId]['history'])>16:
+        chat_holder[conversationId]['history']=[]
     return 'conversation这只是一个回复'
-@app.route("/webhook/images",methods=['POST'])
-def images():
-    proxies={'http':'http://192.168.50.4:7890','https':'https://192.168.50.4:7890'}
-    headers={'Content-Type': 'application/json','Authorization': 'Bearer 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'}
-    json_data=json.loads(request.data)
-    print(json_data['text']['content'])
-    json_message={
-    "prompt": json_data['text']['content'],
-    "n": 1,
-    "size": "1024x1024"}
-    chatgpt_response=requests.post('https://api.openai.com/v1/images/generations',headers=headers,json=json_message,proxies={})
-    json_data['text']['content']=chatgpt_response.json()['data'][0]['url']
-    print("----"*20)
-    print(chatgpt_response.json()['data'][0]['url'])
-    print("----"*20)
-    return json_data
 if __name__ == '__main__':
     print("program loading")
     app.run(host='0.0.0.0',port=18888,debug=True)
