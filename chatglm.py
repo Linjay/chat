d23e14a538aaa9377d1a29b7c9d60ce969743327
linjayzhang <linjayzhang326@gmail.com>
2023-04-22 11:48:49 +0800

    222

--- chatglm.py
+++ chatglm.py
@@ -1,8 +1,7 @@
 import json
-from flask import Flask,request
+from flask import Flask, request
 import requests
 import urllib3
-import time
 
 urllib3.disable_warnings()
 requests.adapters.DEFAULT_RETRIES = 3
@@ -12,68 +11,74 @@ s.verify = False
 urllib3.disable_warnings()
 app = Flask(__name__)
 
-chat_holder={
-    "cid5Jo6E9HFyq9h53OKZLga0Q==":{
-        "history":[],
-        "dingToken":"6aa3cf223f652ff19d48518f9fdd7f7970def822e19a4f30557c6d05d75fbe16"
+chat_holder = {
+    "cid5Jo6E9HFyq9h53OKZLga0Q==": {
+        "history": [],
+        "dingToken": "6aa3cf223f652ff19d48518f9fdd7f7970def822e19a4f30557c6d05d75fbe16"
     },
-    "cidbRFEZ6AtbzXd/9vjVZpLFw==":{
-        "history":[],
-        "dingToken":"0551bff6504ccab407014fc4d8a84c6e8657609f17668fce2c05e2828a903122"
+    "cidbRFEZ6AtbzXd/9vjVZpLFw==": {
+        "history": [],
+        "dingToken": "0551bff6504ccab407014fc4d8a84c6e8657609f17668fce2c05e2828a903122"
     },
-    "cid3woGpG5a4GbH/U88zZVH+w==":{
-        "history":[],
-        "dingToken":"6d796eac1cb8ee7affe06743871a2157c48251149236fa97d53cea2c95787a84"
+    "cid3woGpG5a4GbH/U88zZVH+w==": {
+        "history": [],
+        "dingToken": "6d796eac1cb8ee7affe06743871a2157c48251149236fa97d53cea2c95787a84"
     },
 }
+
 
 @app.route("/")
 def hi():
     return "<p>hi!</p>"
 
-@app.route("/webhook/event",methods=['POST'])
+
+@app.route("/webhook/event", methods=['POST'])
 def event():
     global chat_holder
-    headers={'Content-Type': 'application/json'}
-    json_data=json.loads(request.data)
+    headers = {'Content-Type': 'application/json'}
+    json_data = json.loads(request.data)
     print(json_data)
-    asker=json_data['senderNick']
-    conversationId = json_data['conversationId']
-    if conversationId not in chat_holder.keys():
-        print("invalid conversation:"+conversationId)
-    json_message={
-        "history": chat_holder[conversationId]['history'],
+    asker = json_data['senderNick']
+    conversation_id = json_data['conversationId']
+    if conversation_id not in chat_holder.keys():
+        print("invalid conversation:" + conversation_id)
+    json_message = {
+        "history": chat_holder[conversation_id]['history'],
         "prompt": json_data['text']['content'],
-        "max_length":20480
+        "max_length": 20480
     }
     print("send message to gpt")
     print(json_message)
 
-    answer="不知道！"
+    answer = ""
     try:
-        chatgpt_response=requests.post('http://192.168.50.185:8000',headers=headers,json=json_message,proxies={})
+        chatgpt_response = requests.post('http://192.168.50.185:8000', headers=headers, json=json_message, proxies={})
         print("chatgpt_response:")
         print(chatgpt_response.json())
-        answer=chatgpt_response.json()['response']
-        chat_holder[conversationId]['history'] = chatgpt_response.json()['history']
+        answer = chatgpt_response.json()['response']
+        chat_holder[conversation_id]['history'] = chatgpt_response.json()['history']
 
-        print("----"*20)
+        print("----" * 20)
         print(answer)
-        print("----"*20)
+        print("----" * 20)
     except:
-        answer += "我崩溃了。。。呜呜呜"
+        answer += "我好像找不到我的模型了。。。呜呜呜，请联系我的主人帮我开下机，谢谢！"
         print("An exception occurred")
 
-    headers2={'Content-Type':'application/json'}
-    json_sendmessages={"msgtype": "text","text": {"content":asker+"你好!\r\n"+answer+'\r\nconversation:'+str(len(chat_holder[conversationId]['history']))}}
-    url='https://oapi.dingtalk.com/robot/send?access_token='+chat_holder[conversationId]['dingToken']
-    response=requests.post(url,headers=headers2,json=json_sendmessages)
+    headers2 = {'Content-Type': 'application/json'}
+    json_bot_msg = {"msgtype": "text", "text": {
+        "content": asker + "你好!\r\n" + answer + '\r\nconversation:' + str(
+            len(chat_holder[conversation_id]['history']))}}
+    url = 'https://oapi.dingtalk.com/robot/send?access_token=' + chat_holder[conversation_id]['dingToken']
+    response = requests.post(url, headers=headers2, json=json_bot_msg)
     print(response.text)
 
-    if len(chat_holder[conversationId]['history'])>16:
-        chat_holder[conversationId]['history']=[]
+    if len(chat_holder[conversation_id]['history']) > 50:
+        chat_holder[conversation_id]['history'] = []
     return 'conversation这只是一个回复'
+
+
 if __name__ == '__main__':
     print("program loading")
-    app.run(host='0.0.0.0',port=18888,debug=True)
+    app.run(host='0.0.0.0', port=18888, debug=True)
     print("program exit()")
