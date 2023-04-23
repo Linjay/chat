8b9059f0fdf92f11e097c6ce00464f5434b12f62
linjie.zlj <linjie.zlj@alibaba-inc.com>
2023-04-23 19:41:16 +0800

    111

--- base.py
+++ base.py
@@ -1,6 +1,6 @@
 from transformers import AutoTokenizer, AutoModel
-tokenizer = AutoTokenizer.from_pretrained("D:\programs\chatglm\chatglm-6b-model", trust_remote_code=True)
-model = AutoModel.from_pretrained("D:\programs\chatglm\chatglm-6b-model", trust_remote_code=True).half().cuda()
+tokenizer = AutoTokenizer.from_pretrained("..\chatglm-6b-model", trust_remote_code=True)
+model = AutoModel.from_pretrained("..\chatglm-6b-model", trust_remote_code=True).half().cuda()
 model = model.eval()
 response, history = model.chat(tokenizer, "你好", history=[])
 print(response)
